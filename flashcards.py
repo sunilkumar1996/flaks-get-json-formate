7552556f7156003e332c22d380f4ae82e8225589
sunilkumar1996 <sunilrajputdev@gmail.com>
2020-05-10 12:55:31 +0530

    Add new route login

--- flashcards.py
+++ flashcards.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, redirect, request, url_for
 
 from model import db
 
@@ -33,6 +33,17 @@ def count_views():
     global counter
     counter += 1
     return "This page was served " + str(counter) + "times"
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = "Invalid username and password."
+        else:
+            return redirect(url_for('card'))
+    return render_template('login.html', error=error)
 
 
 app.run(debug=True)
